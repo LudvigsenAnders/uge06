2ac033d70108ea0bbc05d38d9d773186142e5b9a
LudvigsenAnders <anders.ludvigsen@specialisterne.dk>
2026-03-12 17:19:36 +0100

    modified:   src/models/sqlalchemy/__init__.py

--- src/models/sqlalchemy/__init__.py
+++ src/models/sqlalchemy/__init__.py
@@ -1,4 +1,3 @@
-from models.sqlalchemy.stations import Station
-from models.sqlalchemy.observations import Observation
-from models.sqlalchemy.ingest_checkpoint import IngestCheckpoint
 from models.sqlalchemy.base import Base
+
+__all__ = ["Base"]
